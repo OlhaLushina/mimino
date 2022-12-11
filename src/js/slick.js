87ae2f55cee5a9d884a62cd417254f74057d0d15
NataliiaRud <nrud1981@gmail.com>
2022-12-11 22:12:23 +0200

    slider not ready

---
+++ src/js/slick.js
@@ -0,0 +1,47 @@
+// $('.one-time').slick({
+//   dots: false,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 2,
+//   slidesToScroll: 3,
+//   adaptiveHeight: true,
+//   arrows: true,
+//   prevArrow:
+//     "<img class='a-left control-c prev slick-prev' src='url(../slick/images/cil_arrow-left.svg)'>",
+//   nextArrow:
+//     "<img class='a-right control-c next slick-next' src='url(../slick/images/cil_arrow-left.svg)'>",
+// });
+
+// $('.slick-slider').slick({
+//   arrows: true,
+//   prevArrow:
+//     "<img class='a-left control-c prev slick-prev' src='url(../slick/images/cil_arrow-right.svg)'>",
+//   nextArrow:
+//     "<img class='a-right control-c next slick-next' src='url(../slick/images/cil_arrow-right.svg)'>",
+// });
+
+// $('.multiple-items').slick({
+//   infinite: false,
+//   slidesToShow: 2,
+//   slidesToScroll: 6,
+//   dots: false,
+//   arrows: true,
+// });
+// $('.variable-width').slick({
+//   dots: false,
+//   infinite: true,
+//   speed: 300,
+//   slidesToShow: 1,
+
+//   centerMode: true,
+//   variableWidth: true,
+//   centerPadding: '60px',
+// });
+$('.one-time').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 2,
+  slidesToScroll: 6,
+  adaptiveHeight: true,
+});
