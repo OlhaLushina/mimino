a44b71ea0c8f515ae65e13c224116f86f80d2b49
Nataliia_Rud <nrud1981@gmail.com>
2022-12-14 21:55:49 +0200

    slider-mob-and-tab-ok

--- src/js/slick.js
+++ src/js/slick.js
@@ -2,6 +2,12 @@ $('.one-time').slick({
   dots: false,
   infinite: true,
   speed: 300,
-  slidesToShow: 1,
+  slidesToShow: 2,
   adaptiveHeight: true,
+});
+$('.multiple-items').slick({
+  infinite: true,
+  slidesToShow: 2,
+  slidesToScroll: 2,
+  variableWidth: true,
 });
