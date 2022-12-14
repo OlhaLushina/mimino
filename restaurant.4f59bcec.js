b44fd173fe33afc8d902f262d6f066796ab4a79e
OlhaLushina <36839924+OlhaLushina@users.noreply.github.com>
2022-12-14 12:20:55 +0000

    Deploying to gh-pages from @ OlhaLushina/mimino@902b48073b5568113fb88297df299a1a47015f52 🚀

--- restaurant.4f59bcec.js
+++ restaurant.4f59bcec.js
@@ -1,2 +1,2 @@
 $(".one-time").slick({dots:!1,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
-//# sourceMappingURL=hotel.5e70b690.js.map
+//# sourceMappingURL=restaurant.4f59bcec.js.map
--- restaurant.7a89db23.js
+++ restaurant.7a89db23.js
@@ -1,2 +1,2 @@
 $(".one-time").slick({dots:!1,infinite:!0,speed:300,slidesToShow:1,adaptiveHeight:!0});
-//# sourceMappingURL=hotel.209ead24.js.map
+//# sourceMappingURL=restaurant.7a89db23.js.map
