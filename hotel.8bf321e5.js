ec3425d04b804a0bf1480ae57f96c12669b1226d
OlhaLushina <36839924+OlhaLushina@users.noreply.github.com>
2022-12-15 23:37:47 +0000

    Deploying to gh-pages from @ OlhaLushina/mimino@807025ac05313b13c84623b35032bf2e9e71e64b 🚀

---
+++ hotel.8bf321e5.js
@@ -0,0 +1,2 @@
+$(".one-time").slick({dots:!1,infinite:!0,slidesToShow:2,adaptiveHeight:!0,speed:2e3,easing:"ease"}),$(".multiple-items").slick({infinite:!0,slidesToShow:2,slidesToScroll:2,variableWidth:!0,speed:2e3,easing:"ease"});
+//# sourceMappingURL=hotel.8bf321e5.js.map
